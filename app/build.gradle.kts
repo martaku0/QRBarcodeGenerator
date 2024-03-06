d022a214e983ac624746051c697ad4aefa1db6ab
martaku0 <marta.l.kurowska@gmail.com>
2024-03-06 13:49:23 +0100

    qr code generate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
 dependencies {
 
+    // https://mavenlibs.com/maven/dependency/com.google.zxing/javase
+    implementation ("com.google.zxing:javase:3.5.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/barcodegenerator/ui/barqr/BarqrFragment.java
+++ app/src/main/java/com/example/barcodegenerator/ui/barqr/BarqrFragment.java
@@ -1,5 +1,7 @@
 package com.example.barcodegenerator.ui.barqr;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.barcodegenerator.databinding.FragmentBarqrBinding;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.util.Hashtable;
 
 public class BarqrFragment extends Fragment {
 
@@ -37,9 +50,9 @@ public class BarqrFragment extends Fragment {
         binding.generateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //generateBarcode();
-                String txt = String.valueOf(binding.textToCode.getText());
-                Log.d("TAG", "onClick: " + txt);
+                String qrcodeTxt = String.valueOf(binding.textToCode.getText());
+                ImageView qrcodeImgView = binding.qrcodeImage;
+                generateBarcode(qrcodeImgView, qrcodeTxt);
             }
         });
     }
@@ -48,5 +61,28 @@ public class BarqrFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void generateBarcode(ImageView imageView, String text) {
+        if(!text.equals("")) {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            try {
+                BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 300, 300);
+                int width = bitMatrix.getWidth();
+                int height = bitMatrix.getHeight();
+                Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        bitmap.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                    }
+                }
+
+                imageView.setImageBitmap(bitmap);
+
+            } catch (WriterException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
