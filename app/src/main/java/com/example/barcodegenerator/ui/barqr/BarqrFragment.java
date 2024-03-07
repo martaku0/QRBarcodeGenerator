937ac5612be42b4df744d1748094aea6289f858c
martaku0 <marta.l.kurowska@gmail.com>
2024-03-07 10:07:33 +0100

    ean13 barcode generator

--- app/src/main/java/com/example/barcodegenerator/ui/barqr/BarqrFragment.java
+++ app/src/main/java/com/example/barcodegenerator/ui/barqr/BarqrFragment.java
@@ -7,10 +7,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -47,12 +50,23 @@ public class BarqrFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Spinner spinner = binding.spinner;
+        String[] values = {"QRCODE", "EAN_13"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         binding.generateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String qrcodeTxt = String.valueOf(binding.textToCode.getText());
                 ImageView qrcodeImgView = binding.qrcodeImage;
-                generateBarcode(qrcodeImgView, qrcodeTxt);
+                if(spinner.getSelectedItemId() == 0){
+                    generateBarcode(qrcodeImgView, qrcodeTxt);
+                }
+                else if(spinner.getSelectedItemId() == 1){
+                    generateBarcodeEAN(qrcodeImgView, qrcodeTxt);
+                }
             }
         });
     }
@@ -63,7 +77,7 @@ public class BarqrFragment extends Fragment {
         binding = null;
     }
 
-    public void generateBarcode(ImageView imageView, String text) {
+    private void generateBarcode(ImageView imageView, String text) {
         if(!text.equals("")) {
             QRCodeWriter qrCodeWriter = new QRCodeWriter();
             try {
@@ -83,6 +97,61 @@ public class BarqrFragment extends Fragment {
             } catch (WriterException e) {
                 e.printStackTrace();
             }
+        }
+        else{
+            Toast.makeText(getActivity(), "Input cannot be empty!",
+                    Toast.LENGTH_LONG).show();
+            imageView.setImageBitmap(null);
+        }
+    }
+
+    private void generateBarcodeEAN(ImageView imageView, String text) {
+        if(text.length() == 13 || text.length() == 12) {
+            if((text.length() == 13 && isChecksumDigitCorrect(text)) || text.length() == 12){
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try {
+                    BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.EAN_13, 300, 100);
+                    int width = bitMatrix.getWidth();
+                    int height = bitMatrix.getHeight();
+                    Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+
+                    for (int x = 0; x < width; x++) {
+                        for (int y = 0; y < height; y++) {
+                            bitmap.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                        }
+                    }
+
+                    imageView.setImageBitmap(bitmap);
+
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                Toast.makeText(getActivity(), "Wrong checksum!",
+                        Toast.LENGTH_LONG).show();
+                        imageView.setImageBitmap(null);
+            }
+        }
+        else{
+            Toast.makeText(getActivity(), "Input should be 12 or 13 char length!",
+                    Toast.LENGTH_LONG).show();
+                    imageView.setImageBitmap(null);
+        }
+    }
+
+    private static boolean isChecksumDigitCorrect(String barcode) {
+        int sum = 0;
+        for (int i = 0; i < 12; i++) {
+            int digit = Character.getNumericValue(barcode.charAt(i));
+            sum += i % 2 == 0 ? digit : digit * 3;
+        }
+        int checksum = (10 - (sum % 10)) % 10;
+        if(checksum == Character.getNumericValue(barcode.charAt(12))){
+            return true;
+        }
+        else{
+            return false;
         }
     }
 }
